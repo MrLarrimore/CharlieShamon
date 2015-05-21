56df0bd2a41ba4fb3067dc58565bc6b7b5bc6b98
Shamon Fowler <shamonlfjr@gmail.com>
2015-05-21 10:38:34 -0700

    no message

--- index.php
+++ index.php
@@ -1,17 +1,59 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<!DOCTYPE HTML>
+<?php?>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        
-        ?>
-    </body>
+	<head>
+		<title>melonJS Template</title>
+		<link rel="stylesheet" type="text/css" media="screen" href="index.css">
+		<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <link rel="apple-touch-icon" href="icons/touch-icon-iphone-60x60.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="icons/touch-icon-ipad-76x76.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="icons/touch-icon-iphone-retina-120x120.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="icons/touch-icon-ipad-retina-152x152.png">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	</head>
+	<body>
+		<!-- Canvas placeholder -->
+		<div id="screen"></div>
+                
+                <form id="input" method="post">
+                    <div class="field">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username" autocomplete="off"> 
+                    </div>
+                    
+                    <div class="password">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password">
+                    </div>
+                    
+                    <button type="button" id="register">Register</button>
+                    <button type="button" id="load">Load</button>
+                    <button type="button" id="mainmenu">Main Menu</button>
+                </form>
+                
+		<!-- melonJS Library -->
+		<!-- build:js js/app.min.js -->
+		<script type="text/javascript" src="lib/melonJS-1.1.0-min.js"></script>
+
+		<!-- Plugin(s) -->
+		<script type="text/javascript" src="lib/plugins/debugPanel.js"></script>
+		
+		<!-- Game Scripts -->
+		<script type="text/javascript" src="js/game.js"></script>
+		<script type="text/javascript" src="js/resources.js"></script>
+                <script type="text/javascript" src="js/entities/boss.js"></script>
+                <script type="text/javascript" src="js/entities/enemy.js"></script>
+                <script type="text/javascript" src="js/entities/player.js"></script>
+                <script type="text/javascript" src="js/screens/gameoverscreen.js"></script>
+                <script type="text/javascript" src="js/screens/loginscreen.js"></script>
+                <script type="text/javascript" src="js/screens/upgradescreen.js"></script>
+                <script type="text/javascript" src="js/screens/titlescreen.js"></script>
+                <script type="text/javascript" src="js/screens/playscreen.js"></script>
+		<!-- /build -->
+        </body>
 </html>
